3ac74eedbe8a90c423c84c66804c1437a11aeadc
Mark Aldrich <robertx.m.aldrich@intel.com>
2015-09-15 17:40:37 -0700

    Changed example to show column datatype change to string

--- engine-plugins/frame-plugins/src/main/scala/org/trustedanalytics/atk/engine/frame/plugins/UnflattenColumnPlugin.scala
+++ engine-plugins/frame-plugins/src/main/scala/org/trustedanalytics/atk/engine/frame/plugins/UnflattenColumnPlugin.scala
@@ -35,7 +35,8 @@ import org.trustedanalytics.atk.domain.DomainJsonProtocol._
 @PluginDoc(oneLine = "Compacts data from multiple rows based on cell data.",
   extended = """Groups together cells in all columns (less the composite key) using "," as string delimiter.
 The original rows are deleted.
-The grouping takes place based on a composite key created from cell values.""")
+The grouping takes place based on a composite key created from cell values.
+The column datatypes are changed to string.""")
 class UnflattenColumnPlugin extends SparkCommandPlugin[UnflattenColumnArgs, UnitReturn] {
 
   private val defaultDelimiter = ","
