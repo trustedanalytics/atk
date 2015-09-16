ae47d8eafac7e1a4117157a254621805794f3101
rodorad <rene.o.dorado@intel.com>
2015-09-16 14:16:05 -0700

    add the versions.js file
    Add the versions.js file that will be used by gh-pages branch to render the menu and link to the correct documentation.

---
+++ doc/versions.js
@@ -0,0 +1,15 @@
+/*
+This file is loaded by the gh-pages branch please do not remove unless you know what you are doing
+*/
+
+/*The default rendered doc*/
+var LATEST_VERSION="v0.4.0";
+
+var VERSIONS = {
+  /*The index and the version need to match a tag in the branch.
+  The same tag should have a corresponding folder in the gh-pages/versions folder.
+  */
+  "v0.4.0": { "type" : "Production", "version": "v0.4.0"},
+  "ww37.5": { "type" : "Weekly" , "version": "ww37.5"},
+  "master": { "type" : "Master" , "version": "NA"}
+}
