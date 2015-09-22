8fea945603a58677849c7ef16eadfcaf304610c8
scatrava <serban.catrava@intel.com>
2015-09-22 14:28:09 -0700

    updated the config for dp2 deployment

--- package/config/trustedanalytics-tar/rest-server.sh
+++ package/config/trustedanalytics-tar/rest-server.sh
@@ -40,6 +40,13 @@ export PG_PASS=$(echo $VCAP_SERVICES | $jq '.postgresql93 | .[0].credentials.pas
 export PG_DB=$(echo $VCAP_SERVICES | $jq '.postgresql93 | .[0].credentials.dbname' | tr -d '"')
 export PG_URL=$(echo $VCAP_SERVICES | $jq '.postgresql93 | .[0].credentials.uri' | tr -d '"')
 
+export POSTGRES_HOST=$PG_HOST
+export POSTGRES_PORT=$PG_PORT
+export POSTGRES_USER=$PG_USER
+export POSTGRES_PASS=$PG_PASS
+export POSTGRES_DB=$PG_DB
+export POSTGRES_URL=$PG_URL
+
 export MYSQL_HOST=$(echo $VCAP_SERVICES | $jq '.mysql56 | .[0].credentials.hostname' | tr -d '"')
 export MYSQL_PORT=$(echo $VCAP_SERVICES | $jq '.mysql56 | .[0].credentials.port' | tr -d '"')
 export MYSQL_USER=$(echo $VCAP_SERVICES | $jq '.mysql56 | .[0].credentials.username' | tr -d '"')
