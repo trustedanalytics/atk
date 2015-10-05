264a01fad3a844b17b4408d77ecfb5ee78825e45
Todd Lisonbee <todd.lisonbee@intel.com>
2015-10-05 00:32:03 -0700

    Changes for running tests on build server

--- integration-tests/rest-server-start.sh
+++ integration-tests/rest-server-start.sh
@@ -5,13 +5,14 @@
 
 NAME="[`basename $0`]"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+export TARGET_DIR=$DIR/target
 
 echo "$NAME DIR=$DIR"
 
 CONFDIR=$DIR/conf
 
 export MAVEN_REPO=~/.m2/repository
-export CP=$DIR/../conf/:/etc/hadoop/conf:/etc/hbase/conf:$DIR/../module-loader/target/module-loader-master-SNAPSHOT.jar:$MAVEN_REPO/org/scala-lang/scala-library/2.10.4/scala-library-2.10.4.jar:$MAVEN_REPO/com/typesafe/config/1.2.1/config-1.2.1.jar:$MAVEN_REPO/org/scala-lang/scala-reflect/2.10.4/scala-reflect-2.10.4.jar
+export CP=$DIR/../conf/:/etc/hadoop/conf:/etc/hbase/conf:$DIR/../module-loader/target/module-loader-master-SNAPSHOT.jar:$MAVEN_REPO/org/scala-lang/scala-library/2.10.4/scala-library-2.10.4.jar:$MAVEN_REPO/com/typesafe/config/1.2.1/config-1.2.1.jar:$MAVEN_REPO/org/scala-lang/scala-reflect/2.10.4/scala-reflect-2.10.4.jar:`ls $TARGET_DIR/dependencies/*.jar | tr '\n' ':' `
 
 export SEARCH_PATH="-Datk.module-loader.search-path=module-loader/target:rest-server/target:engine/engine-core/target:engine/interfaces/target:engine-plugins/frame-plugins/target:engine-plugins/graph-plugins/target:engine-plugins/model-plugins/target:engine-plugins/giraph-plugins/target"
 
@@ -34,7 +35,7 @@ else
     echo "$NAME Port $PORT is free"
 fi
 
-export TARGET_DIR=$DIR/target
+
 export FS_ROOT=$TARGET_DIR/fs-root
 LOG=$TARGET_DIR/rest-server.log
 
@@ -52,7 +53,7 @@ cp -rp $DIR/datasets $FS_ROOT
 echo "$NAME fs.root is $FS_ROOT"
 echo "$NAME Api Server logging going to $LOG"
 
-echo "starting"
+echo "starting, CP=$CP"
 java $@ -XX:MaxPermSize=256m -Xss10m $SEARCH_PATH -cp "$CONF:$CP" \
     -Dconfig.resource=integration-test.conf \
     -Dtrustedanalytics.atk.engine.fs.root=file:$FS_ROOT \
