3fa4fbef295ab6889fdd8ab34bfd904137be3862
Todd Lisonbee <todd.lisonbee@intel.com>
2015-10-06 18:46:26 -0700

    Updating SPARK_HOME for build server

--- integration-tests/rest-server-start.sh
+++ integration-tests/rest-server-start.sh
@@ -12,7 +12,12 @@ echo "$NAME DIR=$DIR"
 CONFDIR=$DIR/conf
 
 # was needed for Python UDF tests to pass locally
-export SPARK_HOME=/opt/cloudera/parcels/CDH/lib/spark/
+if [ -d "/usr/lib/spark/spark-cdh5.4.2-release/" ]
+then
+    export SPARK_HOME=/usr/lib/spark/spark-cdh5.4.2-release/
+else
+    export SPARK_HOME=/opt/cloudera/parcels/CDH/lib/spark/
+fi
 
 export MAVEN_REPO=~/.m2/repository
 export CP=$DIR/../conf/:/etc/hadoop/conf:/etc/hbase/conf:$DIR/../module-loader/target/module-loader-master-SNAPSHOT.jar:$MAVEN_REPO/org/scala-lang/scala-library/2.10.4/scala-library-2.10.4.jar:$MAVEN_REPO/com/typesafe/config/1.2.1/config-1.2.1.jar:$MAVEN_REPO/org/scala-lang/scala-reflect/2.10.4/scala-reflect-2.10.4.jar:`ls $TARGET_DIR/dependencies/*.jar | tr '\n' ':' `
