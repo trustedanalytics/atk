f78db64d9cc3acd6c2682e7f241cb344e98908e2
Todd Lisonbee <todd.lisonbee@intel.com>
2015-10-14 19:36:24 -0700

    Updating search path for local development

--- bin/rest-server.sh
+++ bin/rest-server.sh
@@ -5,10 +5,12 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 export MAVEN_REPO=~/.m2/repository
 export CP=$DIR/../conf/:/etc/hadoop/conf:/etc/hbase/conf:$DIR/../module-loader/target/module-loader-master-SNAPSHOT.jar:$MAVEN_REPO/org/scala-lang/scala-library/2.10.4/scala-library-2.10.4.jar:$MAVEN_REPO/com/typesafe/config/1.2.1/config-1.2.1.jar:$MAVEN_REPO/org/scala-lang/scala-reflect/2.10.4/scala-reflect-2.10.4.jar
 
+export BASEDIR=$DIR/..
+
 pushd $DIR/..
 pwd
 
-export SEARCH_PATH="-Datk.module-loader.search-path=${DIR}/..:${HOME}/.m2/"
+export SEARCH_PATH="-Datk.module-loader.search-path=${BASEDIR}/rest-server:${BASEDIR}/engine:${BASEDIR}/engine-plugins:${BASEDIR}/model-publish-format:${HOME}/.m2/"
 export HOSTNAME=`hostname`
 export YARN_CONF_DIR="/etc/hadoop/conf"
 
