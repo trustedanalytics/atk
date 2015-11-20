46eebaf1089ed44325cdd53ebc935f603484720d
rodorad <rene.o.dorado@intel.com>
2015-11-20 15:57:55 -0800

    pushd to root dir before executing start script

--- doc/start-server.sh
+++ doc/start-server.sh
@@ -9,11 +9,13 @@ do
         pid=$(ps -aux  | grep -v grep | grep "java.*org.trustedanalytics.atk.moduleloader.Module" | head -n 1 |  awk '{print $2}')
 
 done
-nohup ../bin/rest-server.sh > out 2>&1&
-PID=$!
+pushd ..
+  nohup bin/rest-server.sh > out 2>&1&
+  PID=$!
+popd
 
 echo "PID: " $PID
-max=20
+max=10
 count=0
 
 while [ $count -lt $max ];
@@ -29,3 +31,7 @@ do
     sleep 2
     count=$((count+1))
 done
+
+tail ../out
+
+netstat -tulnp
