8128983634e138657b8a742e23f07a4041700b2c
Joyesh Mishra <joyesh.mishra@intel.com>
2015-11-25 21:50:10 -0800

    Changed code per code review comments

--- engine/interfaces/src/main/scala/org/trustedanalytics/atk/domain/datacatalog/TapDataCatalogResponse.scala
+++ engine/interfaces/src/main/scala/org/trustedanalytics/atk/domain/datacatalog/TapDataCatalogResponse.scala
@@ -19,7 +19,7 @@ package org.trustedanalytics.atk.domain.datacatalog
 /**
  * Response class for the data catalog entries
  */
-case class IndexedMetadataEntryWithID(id: String,
+case class TapDataCatalogResponse(id: String,
                                       size: Int,
                                       title: String,
                                       dataSample: String,
@@ -31,11 +31,26 @@ case class IndexedMetadataEntryWithID(id: String,
                                       format: String,
                                       creationTime: String,
                                       sourceUri: String)
+
+/*
+ * Request class to add an entry to data catalog
+ */
+case class TAPAddToDataCatalogRequest(title: String,
+                                      category: String,
+                                      recordCount: Int,
+                                      sourceUri: String,
+                                      size: Int,
+                                      orgUUID: String,
+                                      targetUri: String,
+                                      format: String,
+                                      dataSample: String,
+                                      isPublic: Boolean)
+
 /*
  * Catalog Metadata class being returned by plugins. If any plugin returns an object of type CatalogMetadata,
  * python client will send a publish request to add the entry to data catalog
  */
-case class CatalogMetadata(title: String,
+case class ExportMetadata(title: String,
                            size: Int,
                            dataSample: String,
                            recordCount: Long,
@@ -47,25 +62,11 @@ case class CatalogMetadata(title: String,
 
 
 /*
- * Request class to add an entry to data catalog
- */
-case class InputMetadataEntry(title: String,
-                              category: String,
-                              recordCount: Int,
-                              sourceUri: String,
-                              size: Int,
-                              orgUUID: String,
-                              targetUri: String,
-                              format: String,
-                              dataSample: String,
-                              isPublic: Boolean)
-
-/*
  Converts a catalog metadata object (which is handled by ATK) to an input metadata entry for the data catalog service
  */
-object ConvertCatalogMetadataToInputMetadataEntry {
-  def convert(c: CatalogMetadata, orgUUID: String) = {
-    InputMetadataEntry(c.title, c.category, c.recordCount.toInt, c.sourceUri, c.size, orgUUID,
+object ConvertExportMetadataToInputMetadataEntry {
+  def convert(c: ExportMetadata, orgUUID: String) = {
+    TAPAddToDataCatalogRequest(c.title, c.category, c.recordCount.toInt, c.sourceUri, c.size, orgUUID,
       c.targetUri, c.format, c.dataSample, c.isPublic)
   }
 
