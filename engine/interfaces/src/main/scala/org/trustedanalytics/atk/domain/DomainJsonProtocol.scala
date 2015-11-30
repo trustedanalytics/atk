8adc38e675aaf45f433b2c769b1fa966536230e8
Todd Lisonbee <todd.lisonbee@intel.com>
2015-11-30 00:11:15 -0800

    Simplifying error class since most fields were unused

--- engine/interfaces/src/main/scala/org/trustedanalytics/atk/domain/DomainJsonProtocol.scala
+++ engine/interfaces/src/main/scala/org/trustedanalytics/atk/domain/DomainJsonProtocol.scala
@@ -342,7 +342,7 @@ object DomainJsonProtocol extends AtkDefaultJsonProtocol with EventLogging {
   implicit val groupByColumnFormat = jsonFormat3(GroupByArgs)
   implicit val copyWhereFormat = jsonFormat2(CountWhereArgs)
 
-  implicit val errorFormat = jsonFormat5(Error)
+  implicit val errorFormat = jsonFormat2(Error)
   implicit val flattenColumnLongFormat = jsonFormat3(FlattenColumnArgs)
   implicit val unflattenColumnLongFormat = jsonFormat3(UnflattenColumnArgs)
   implicit val dropDuplicatesFormat = jsonFormat2(DropDuplicatesArgs)
