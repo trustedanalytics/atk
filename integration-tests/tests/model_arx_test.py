fbee874e1a4ed306dbb86f0926d8758930dd558c
dmsuehir <dina.m.suehiro@intel.com>
2016-03-03 15:15:05 -0800

    Updating license headers

--- integration-tests/tests/model_arx_test.py
+++ integration-tests/tests/model_arx_test.py
@@ -1,7 +1,7 @@
 # vim: set encoding=utf-8
 
 #
-#  Copyright (c) 2016 Intel Corporation 
+#  Copyright (c) 2015 Intel Corporation 
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
