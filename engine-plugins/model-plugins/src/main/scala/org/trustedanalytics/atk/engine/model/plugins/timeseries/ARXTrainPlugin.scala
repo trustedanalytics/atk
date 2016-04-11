b9815136f99f0c6958f1119878d79676ead35db2
dmsuehir <dina.m.suehiro@intel.com>
2016-04-11 10:45:17 -0700

    Documentation formatting

--- engine-plugins/model-plugins/src/main/scala/org/trustedanalytics/atk/engine/model/plugins/timeseries/ARXTrainPlugin.scala
+++ engine-plugins/model-plugins/src/main/scala/org/trustedanalytics/atk/engine/model/plugins/timeseries/ARXTrainPlugin.scala
@@ -47,8 +47,9 @@ import scala.collection.mutable.ArrayBuffer
 
 #)  Dataset being trained must be small enough to be worked with on a single node.
 #)  If the specified set of exogenous variables is not invertible, there will be an
-error specifying that the matrix is singular.  This happens when there are certain
-patterns in the dataset or columns of all zeros.  """,
+    error specifying that the matrix is singular.  This happens when there are certain
+    patterns in the dataset or columns of all zeros.
+""",
   returns = """A dictionary with trained ARX model with the following keys\:
 
               |   **c** : *float*
