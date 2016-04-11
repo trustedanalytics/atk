2bf533f7b8f1bca9d9fbf1e04afd37910c24c216
Anjali Sood <anjali.sood@intel.com>
2016-04-11 13:50:01 -0700

    updated LDA unit tests

--- scoring-models/src/test/scala/org/trustedanalytics/atk/scoring/models/LdaScoreModelTest.scala
+++ scoring-models/src/test/scala/org/trustedanalytics/atk/scoring/models/LdaScoreModelTest.scala
@@ -56,24 +56,22 @@ class LdaScoreModelTest extends FlatSpec with Matchers with ScalaFutures {
     }
 
     scores.length should equal(3)
-    val score0 = scores(0).asInstanceOf[Map[String, Any]]
-    val score1 = scores(1).asInstanceOf[Map[String, Any]]
-    val score2 = scores(2).asInstanceOf[Map[String, Any]]
+    val score0 = scores(0).asInstanceOf[Array[Any]]
+    val score1 = scores(1).asInstanceOf[Array[Any]]
+    val score2 = scores(2).asInstanceOf[Array[Any]]
 
-    //val score01 = score0(1).asInstanceOf[Map[String, Any]]
-    score0("topics_given_doc").asInstanceOf[List[Double]].toArray should equalWithTolerance(Array(0.5, 0.333333))
-    score0("new_words_count").asInstanceOf[Int] should equal(1)
-    score0("new_words_percentage").asInstanceOf[Double] should equal(100 / 6d +- epsilon)
+    score0(0).asInstanceOf[Vector[Double]].toArray should equalWithTolerance(Array(0.5, 0.333333))
+    score0(1).asInstanceOf[Int] should equal(1)
+    score0(2).asInstanceOf[Double] should equal(100 / 6d +- epsilon)
 
-    //    val score11 = score1(1).asInstanceOf[Map[String, Any]]
-    //    score11("topics_given_doc").asInstanceOf[List[Double]].toArray should equalWithTolerance(Array(0.4375, 0.5625))
-    //    score11("new_words_count").asInstanceOf[Int] should equal(0)
-    //    score11("new_words_percentage").asInstanceOf[Double] should equal(0d +- epsilon)
-    //
-    //    val score21 = score2(1).asInstanceOf[Map[String, Any]]
-    //    score21("topics_given_doc").asInstanceOf[List[Double]].toArray should equalWithTolerance(Array(0d, 0d))
-    //    score21("new_words_count").asInstanceOf[Int] should equal(0)
-    //    score21("new_words_percentage").asInstanceOf[Double] should equal(0d +- epsilon)
+    score1(0).asInstanceOf[Vector[Double]].toArray should equalWithTolerance(Array(0.4375, 0.5625))
+    score1(1).asInstanceOf[Int] should equal(0)
+    score1(2).asInstanceOf[Double] should equal(0d +- epsilon)
+
+    score2(0).asInstanceOf[Vector[Double]].toArray should equalWithTolerance(Array(0d, 0d))
+    score2(1).asInstanceOf[Int] should equal(0)
+    score2(2).asInstanceOf[Double] should equal(0d +- epsilon)
+
   }
 
 }
