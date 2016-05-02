901083e829d29d9f98717335d48b6384808ed696
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2016-05-02 14:47:06 -0700

    Commented out DenseVector and SparseVector tests

--- engine-plugins/model-plugins/src/test/scala/org/apache/spark/mllib/atk/plugins/MLLibJsonProtocolTest.scala
+++ engine-plugins/model-plugins/src/test/scala/org/apache/spark/mllib/atk/plugins/MLLibJsonProtocolTest.scala
@@ -34,53 +34,53 @@ import org.trustedanalytics.atk.scoring.models.{ SVMData, KMeansData }
 import spray.json._
 
 class MLLibJsonProtocolTest extends WordSpec {
-//    "DenseVectorFormat" should {
-//
-//      "be able to serialize" in {
-//        val dv = new DenseVector(Array(1.2, 3.4, 2.2))
-//        assert(dv.toJson.compactPrint == "{\"values\":[1.2,3.4,2.2]}")
-//      }
-//
-//      "parse json" in {
-//        val string =
-//          """
-//            |{
-//            |   "values": [1.2,3.4,5.6,7.8]
-//            |
-//            |
-//            |}
-//          """.stripMargin
-//        val json = JsonParser(string).asJsObject
-//        val dv = json.convertTo[DenseVector] assert(dv.values.length == 4)
-//      }
-//    }
-//
-//    "SparseVectorFormat" should {
-//
-//      "be able to serialize" in {
-//        val sv = new SparseVector(3, Array(1, 2, 3), Array(1.5, 2.5, 3.5))
-//        assert(sv.toJson.compactPrint == "{\"size\":3,\"indices\":[1,2,3],\"values\":[1.5,2.5,3.5]}")
-//      }
-//
-//      "parse json" in {
-//        val string =
-//          """
-//          |{
-//          |   "size": 4,
-//          |   "indices": [1,2,3,4],
-//          |   "values": [1.5,2.5,3.5,4.5]
-//          |
-//          |
-//          | }
-//        """.
-//            stripMargin
-//        val json = JsonParser(string).asJsObject
-//        val sv = json.convertTo[SparseVector]
-//        assert(sv.size == 4)
-//        assert(sv.indices.length == 4)
-//        assert(sv.values.length == 4)
-//      }
-//    }
+  //    "DenseVectorFormat" should {
+  //
+  //      "be able to serialize" in {
+  //        val dv = new DenseVector(Array(1.2, 3.4, 2.2))
+  //        assert(dv.toJson.compactPrint == "{\"values\":[1.2,3.4,2.2]}")
+  //      }
+  //
+  //      "parse json" in {
+  //        val string =
+  //          """
+  //            |{
+  //            |   "values": [1.2,3.4,5.6,7.8]
+  //            |
+  //            |
+  //            |}
+  //          """.stripMargin
+  //        val json = JsonParser(string).asJsObject
+  //        val dv = json.convertTo[DenseVector] assert(dv.values.length == 4)
+  //      }
+  //    }
+  //
+  //    "SparseVectorFormat" should {
+  //
+  //      "be able to serialize" in {
+  //        val sv = new SparseVector(3, Array(1, 2, 3), Array(1.5, 2.5, 3.5))
+  //        assert(sv.toJson.compactPrint == "{\"size\":3,\"indices\":[1,2,3],\"values\":[1.5,2.5,3.5]}")
+  //      }
+  //
+  //      "parse json" in {
+  //        val string =
+  //          """
+  //          |{
+  //          |   "size": 4,
+  //          |   "indices": [1,2,3,4],
+  //          |   "values": [1.5,2.5,3.5,4.5]
+  //          |
+  //          |
+  //          | }
+  //        """.
+  //            stripMargin
+  //        val json = JsonParser(string).asJsObject
+  //        val sv = json.convertTo[SparseVector]
+  //        assert(sv.size == 4)
+  //        assert(sv.indices.length == 4)
+  //        assert(sv.values.length == 4)
+  //      }
+  //    }
 
   "KmeansModelFormat" should {
 
